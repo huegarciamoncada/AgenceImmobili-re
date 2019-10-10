c4910377391b22389e80b15d7cee9754918d4255
huegarciamoncada <nttfue@gmail.com>
2019-10-10 12:41:50 +0200

    deuxième commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class HomeController{
+    public function index():Response
+    {
+        return new Response('Salut les gen');
+    }
+}
+?>
