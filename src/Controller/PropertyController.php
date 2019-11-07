be7490a72ab52dcee731f3a78b67bd1a311225a3
huegarciamoncada <nttfue@gmail.com>
2019-11-07 17:59:44 +0100

    feature front-end

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,17 +1,50 @@
 <?php
 namespace App\Controller;
+use App\Entity\Property;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class PropertyController{
+
+class PropertyController extends AbstractController{
+    
     /**
-     * @var Environment
-     */
+    * @Route("/biens", name="property.index")
+      */
+    public function index():Response
+    {
+        /* tao entity ghi vao DB
+        $property = new Property();
+        $property->setTitle('Mon premier Appartement')
+        ->setPrice(200000)
+        ->setRooms(4)
+        ->setBedrooms(3)
+        ->setDescription('une petite description')
+        ->setSurface(60)
+        ->setFloor(4)
+        ->setHeat(1)
+        ->setCity('Montpellier')
+        ->setAddress('15 rue de Victor Hugo')
+        
+        ->setPostalCode('34000');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($property);
+        $em->flush();*/
+        $repository = $this->getDoctrine()->getRepository(Property::class);
+        dump($repository);
+
+
+        return $this->render('property/index.html.twig',['current_menu' => 'properties']);
+    }
+    /*
+     
     private $twig;
     public function __construct($twig){
         $this->twig = $twig;
     }
-    public function index():Response
+    /*public function index():Response
     {
-        return new Response($this->twig->render('property/index.html.twig'));
-    }
+        return new  Response($this->twig->render('pages/index.html.twig'));
+}*/
 }
